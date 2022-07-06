b59f4662997ee0426a141a3c959f9f6d145e6300
patrick mugambi <patrick@kali.pm.com>
2022-07-06 06:28:53 +0300

    add api url

--- src/Login.js
+++ src/Login.js
@@ -5,7 +5,7 @@ import { Container } from "react-bootstrap"
 import Cards from "./Cards";
 import "./index.css"
 const AUTH_URL =
-  "https://accounts.spotify.com/authorize?client_id=9b09f413060c41a1939a4a983f09d3c2&response_type=code&redirect_uri=https://music-backend4.herokuapp.com/&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state"
+  "https://accounts.spotify.com/authorize?client_id=9b09f413060c41a1939a4a983f09d3c2&response_type=code&redirect_uri=http://music-backend4.herokuapp.com/&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state"
 
 export default function Login() {
   return (
